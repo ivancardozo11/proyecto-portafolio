481d0de8ac0ee5aa9e8fde907bc3be8a9e87e5cd
ivancardozo11 <ivancardozo11@hotmail.com>
2018-09-28 16:07:44 -0300

    Portafolio funcionando ok

---
+++ wp-content/themes/index.php
@@ -0,0 +1,50 @@
+<?php
+// Silence is golden.
+<section id="Contact-form">
+  <br>
+      <div class="container">
+        <div class="contact-header" align="center">
+          <h2>Cotact Form</h2>
+          <p>Lorem ipsum dolor sit amet</p>
+        </div>
+        <!-- Form  -->
+        <form>
+          <div class="row">
+            <div class="col-md-4 col-sm-4">
+                <h4 align="center"><strong>Company Name,</strong></h4>
+                <p align="center"><strong>Address:</strong>  Abcd, Near XYZ,<br>Country<br><br><strong>Contact:</strong> +91-123456987</p>
+              </div>
+            <div class="col-md-4 col-sm-4 ">
+                <div class="form-group">
+                  <label for="exampleInputUsername">Name</label>
+                  <input type="email" class="form-control" id="exampleInputUsername" placeholder=" Enter Your Name">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmailId">Email-id</label>
+                  <input type="password" class="form-control" id="exampleInputEmailId" placeholder="Enter email-id">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputnumber">Mobile-no</label>
+                  <input type="password" class="form-control" id="exampleInputnumber" placeholder="Enter 10 Digit Mobile-no ">
+                </div>
+
+              </div>
+              <div class="col-md-4 col-sm-4">
+                <div class="form-group">
+                  <label for="exampleInputtext">Subject</label>
+                  <input type="password" class="form-control" id="exampleInputtext" placeholder="Enter Message title">
+                </div>
+                <div class="form-group">
+                  <label for="inputMessage">Message</label>
+                  <textarea  class="form-control" for="inputMessage" placeholder="Enter Your Message"></textarea>
+                </div>
+                <div>
+                  <button type="button" class="btn btn-md btn-default submit"><i class="fa fa-paper-plane" aria-hidden="true"></i> Submit Message</button>
+                </div>
+                <br>
+              </div>
+
+          </div>
+        </form>
+
+  </section>
